a681955203390f877adbfcba154aeb2e8d0edee3
Thiago Reis <reist36@gmail.com>
2022-10-19 00:06:30 -0300

    Adding CollectionsSet ativities

---
+++ CollectionsSet/src/main/java/ExemploSet.java
@@ -0,0 +1,199 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class ExemploSet {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Crie um conjunto e adicione as notas: ");
+        Set<Double> notas = new HashSet<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
+        System.out.println(notas.toString());
+
+        //System.out.println("Exiba a posição da nota 5.0"); // Não é possível pesquisar pela posição no Set
+        //System.out.println("Adicione na lista a nota 8.0 na posição 4: "); // Não é possível trabalhar com posições no Set
+        //System.out.println("Substitua a nota 5.0 pela nota 6.0: "); // Também não é possível no Set
+
+        System.out.println("Confira se a nota 5.0 está no conjunto: " + notas.contains(5d));
+        //System.out.println("Exiba a terceira nota adicionada: "); // Não é possível pois não existe no Set o método Get para passar o index para retornar a nota.
+
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+        System.out.println("Exiba a maior nota: " + Collections.max(notas));
+
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0.0;
+        while(iterator.hasNext()) {
+            Double next = iterator.next();
+            soma += next;
+        }
+        System.out.println("Exiba a soma dos valores: " + soma);
+
+        System.out.println("Exiba a média das notas: " + (soma / notas.size()));
+
+        System.out.println("Remove a nota 0: ");
+        notas.remove(0d);
+        System.out.println(notas);
+
+        System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+        Iterator<Double> iterator1 = notas.iterator();
+        while(iterator1.hasNext()){
+            Double next = iterator1.next();
+            if (next < 7) iterator1.remove();
+        }
+        System.out.println(notas);
+
+        System.out.println("Exiba todas as notas na ordem em que foram informadas: ");
+        Set<Double> notas2 = new LinkedHashSet<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
+        System.out.println(notas2);
+
+        System.out.println("Exiba todas as notas na ordem crescente: ");//Utiliza o TreeSet pois ele organiza em ordem natural dos elementos
+        Set<Double> notas3 = new TreeSet<>(notas2);
+        System.out.println(notas3);
+
+        System.out.println("Apague todo o conjunto: ");
+        notas.clear();
+
+        System.out.println("Confira se o conjunto está vazio: " + notas.isEmpty());
+        System.out.println("Confira se o conjunto 2 está vazio: " + notas2.isEmpty());
+        System.out.println("Confira se o conjunto 3 está vazio: " + notas3.isEmpty());
+
+        System.out.println();
+        System.out.println("---- ORDENAÇÃO EXEMPLOS: ----");
+
+        System.out.println("Ordem Aleatória");
+        Set<Serie> minhasSeries = new HashSet<>(){{
+            add(new Serie("Anéis do poder", "Ficção", 60));
+            add(new Serie("House Of Dragons", "Fantasia", 60));
+            add(new Serie("Roda do Tempo", "Drama", 60));
+        }};
+
+        for (Serie serie : minhasSeries) System.out.println(serie.getNome() + " - "
+                + serie.getGenero() + " - " + serie.getTempoEpisodio());
+
+        System.out.println("\nOrdem Inserção");
+        Set<Serie> minhasSeries1 = new LinkedHashSet<>(){{
+            add(new Serie("Peaky Blinders", "Ação", 50));
+            add(new Serie("Breaking Bad", "Ação", 60));
+            add(new Serie("One piece", "Animação", 25));
+        }};
+
+        for (Serie serie : minhasSeries1) System.out.println(serie.getNome() + " - "
+                + serie.getGenero() + " - " + serie.getTempoEpisodio());
+
+        System.out.println("\nOrdem Natural (Tempo Episódio)");
+        Set<Serie> minhasSeries2 = new TreeSet<>(minhasSeries1);
+        for (Serie serie : minhasSeries2) System.out.println(serie.getNome() + " - "
+                + serie.getGenero() + " - " + serie.getTempoEpisodio());
+
+        System.out.println("\nOrdem Nome/Gênero/TempoEpisodio");
+        Set<Serie> minhasSeries3 = new TreeSet<>(new ComparatorNomeGeneroTempoEpisodio());
+        minhasSeries3.addAll(minhasSeries);
+        for (Serie serie : minhasSeries3) System.out.println(serie.getNome() + " - "
+                + serie.getGenero() + " - " + serie.getTempoEpisodio());
+    }
+}
+
+class Serie implements Comparable<Serie>{
+    private String nome;
+    private String genero;
+    private Integer tempoEpisodio;
+
+    public Serie(String nome, String genero, Integer tempoEpisodio) {
+        this.nome = nome;
+        this.genero = genero;
+        this.tempoEpisodio = tempoEpisodio;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Integer getTempoEpisodio() {
+        return tempoEpisodio;
+    }
+
+    public void setTempoEpisodio(Integer tempoEpisodio) {
+        this.tempoEpisodio = tempoEpisodio;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + "nome=" + nome + ", genero=" + genero + ", tempoEpisodio=" + tempoEpisodio + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + Objects.hashCode(this.nome);
+        hash = 67 * hash + Objects.hashCode(this.genero);
+        hash = 67 * hash + Objects.hashCode(this.tempoEpisodio);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Serie other = (Serie) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.genero, other.genero)) {
+            return false;
+        }
+        return Objects.equals(this.tempoEpisodio, other.tempoEpisodio);
+    }
+
+    @Override
+    public int compareTo(Serie serie) {
+        int tempoEpisodio = Integer.compare(this.getTempoEpisodio(), serie.getTempoEpisodio());
+        if (tempoEpisodio != 0) return tempoEpisodio;
+
+        return this.getGenero().compareTo(serie.getGenero());
+    }
+}
+
+class ComparatorNomeGeneroTempoEpisodio implements Comparator<Serie>{
+
+    @Override
+    public int compare(Serie s1, Serie s2) {
+        int nome = s1.getNome().compareTo(s2.getNome());
+        if (nome != 0) return nome;
+
+        int genero = s1.getGenero().compareTo(s2.getGenero());
+        if (genero != 0) return genero;
+
+        int tempoEpisodio = Integer.compare(s1.getTempoEpisodio(), s2.getTempoEpisodio());
+
+        return 0;
+    }
+//11:10 min
+}
---
+++ ExerciciosList/src/main/java/main.java
@@ -0,0 +1,23 @@
+public class main {
+    public static void main(String[] args) {
+        // TODO: Faça um programa que receba a temperatura média dos 6
+        //primeiros meses do ano e armazene-as em uma lista.
+        //Após isto, calcule a média semestral das temperaturas e
+        //mostre todas as temperaturas acima desta média, e em que
+        //mês elas ocorreram (mostrar o mês por extenso: 1 — Janeiro, 2
+        //— Fevereiro e etc).
+
+
+        // TODO:  Utilizando listas, faça um programa que faça 5 perguntas para uma
+        //pessoa sobre um crime. As perguntas são:
+        //1. “Telefonou para a vítima?"
+        //2. “Esteve no local do crime?"
+        //3. “Mora perto da vítima?"
+        //4. "Devia para a vítima?"
+        //5. "Já trabalhou com a vítima?"
+        //Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre
+        //3 e 4 como "Cúmplice" e 5 como "Assassina". Caso contrário, ela será classificada como "Inocente".
+
+
+    }
+}
