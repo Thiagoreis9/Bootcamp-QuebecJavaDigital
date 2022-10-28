41256b3ce6d396b66ada098baca3eaffd47361f7
Thiago_Reis <reist36@gmail.com>
2022-10-28 11:13:51 -0300

    Adding my first project with spring initializr

---
+++ PrimeiroProjetoSpring/src/main/java/br/com/PrimeiroProjetoSpring/PrimeiroProjetoSpring/PrimeiroProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.PrimeiroProjetoSpring.PrimeiroProjetoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ PrimeiroProjetoSpring/src/test/java/br/com/PrimeiroProjetoSpring/PrimeiroProjetoSpring/PrimeiroProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.PrimeiroProjetoSpring.PrimeiroProjetoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
